92856f19ed0f9a6c90d44ae3d0e32679b3e3cb6d
Brady Pitts <this.is.bp67c@gmail.com>
2021-04-12 15:13:18 -0600

    finished but buggy

--- src/components/TopicBrowser/TopicBrowser.js
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -1,5 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react';
+import EvenAndOdd from '../Topics/EvenAndOdd';
+import FilterObject from '../Topics/FilterObject';
+import FilterString from '../Topics/FilterString';
+import Palindrom from '../Topics/Palindrome';
+import Sum from '../Topics/Sum';
 
-const TopicBrowser = () => <p>Hello World</p>
+class TopicBrowser extends Component{
+  render(){
+    return(
+      <div>
+        <EvenAndOdd />,
+        <FilterObject />,
+        <FilterString />,
+        <Palindrom />,
+        <Sum />
+      </div>
+    )
+  }
+}
 
 export default TopicBrowser
